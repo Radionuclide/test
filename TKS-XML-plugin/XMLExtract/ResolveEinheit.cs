96e333ba63c52419f067baed66b9810b466e7377
Dominik Guder <dominik.guder@iba-ag.com>
2013-04-22 12:47:39 +0000

    check existing Einheit enum entries before special ones
    svn path=/branches/xmlplugin/; revision=197

--- TKS-XML-plugin/XMLExtract/ResolveEinheit.cs
+++ TKS-XML-plugin/XMLExtract/ResolveEinheit.cs
@@ -10,32 +10,29 @@ namespace XmlExtract
     internal class ResolveEinheit
     {
         readonly static Dictionary<string, EinheitEnum> _map = new Dictionary<string, EinheitEnum>() { 
-            { "mm", EinheitEnum.mm },
             { "µm", EinheitEnum.mikrom },
             { "gradC", EinheitEnum.gradC },
             { "m/min", EinheitEnum.mmin },
-            { "kW", EinheitEnum.kW },
-            { "A", EinheitEnum.A },
-            { "V", EinheitEnum.V },
             { "g/qm", EinheitEnum.gqm },
             { "%", EinheitEnum.Prozent },
-            { "KN", EinheitEnum.KN },
-            { "mbar", EinheitEnum.mbar },
-            { "grad", EinheitEnum.grad },
             { "Keine/1", EinheitEnum.Keine1 },
+            { "int", EinheitEnum.Keine1 },
             { "", EinheitEnum.Keine1 },
-            { "min", EinheitEnum.min },
-            { "m", EinheitEnum.m },
             { "As/m²", EinheitEnum.Asqm },
             { "U/min", EinheitEnum.Umin },
-            { "Test", EinheitEnum.Test },
         };
 
         public static object Parse(string unit)
         {
             EinheitEnum einheit;
+            if (Enum<EinheitEnum>.TryParse(unit, false, out einheit))
+                return einheit;
+
             if (_map.TryGetValue(unit, out einheit))
                 return einheit;
+
+            if (unit.StartsWith("[") && unit.EndsWith("]"))
+                return EinheitEnum.Keine1;
 
             return unit;
         }
