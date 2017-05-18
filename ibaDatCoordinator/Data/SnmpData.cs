f008cca5b16d098b289a455689c3d07a0960e9e5
Roman Kolesnik <lesnik4u@yandex.ru>
2017-05-18 09:57:52 +0000

    Added SnmpData.cs draft
    svn path=/branches/snmp/; revision=507

---
+++ ibaDatCoordinator/Data/SnmpData.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using IbaSnmpLib;
+
+namespace iba.Data
+{
+    [Serializable]
+    public class SnmpData : ICloneable
+    {
+        private IbaSnmp _ibaSnmp = null;
+        /// <summary> reference to the SNMP agent engine </summary>
+        public IbaSnmp IbaSnmp
+        {
+            get { return _ibaSnmp; }
+            set { _ibaSnmp = value; }
+        }
+
+        private bool _restartByDefault = false;
+        public bool RestartByDefault
+        {
+            get { return _restartByDefault; }
+            set { _restartByDefault = value; }
+        }
+
+
+        public SnmpData()
+        {
+        }
+
+        /// <summary> reference to the SNMP agent engine </summary>
+        
+        public object Clone()
+        {
+            //todo check
+            object newobj = MemberwiseClone();
+            return newobj;
+        }
+    }
+}
