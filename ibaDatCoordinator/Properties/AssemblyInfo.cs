c8dc1c8b48537d3ac0625c67c653dd8021b08fc3
Michael Verschaeve <michael.verschaeve@iba-ag.com>
2017-11-28 16:52:47 +0000

    stuff
    svn path=/trunk/; revision=687

--- ibaDatCoordinator/Properties/AssemblyInfo.cs
+++ ibaDatCoordinator/Properties/AssemblyInfo.cs
@@ -43,7 +43,7 @@ namespace iba
         public static string GetVersion()
         {
             string ver = typeof(iba.MainForm).Assembly.GetName().Version.ToString(3);
-            ver = ver + " BETA6";
+            //ver = ver;
             return ver;
         }
 
