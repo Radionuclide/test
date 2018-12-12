954775e2932bd6f35ca6a349c18a0d5e11faa5bd
Tomes Stoop <tomes.stoop@iba-ag.com>
2018-12-12 14:54:45 +0000

    [2.1.0 BETA2]
    svn path=/trunk/; revision=759

--- ibaDatCoordinator/Properties/AssemblyInfo.cs
+++ ibaDatCoordinator/Properties/AssemblyInfo.cs
@@ -43,7 +43,7 @@ namespace iba
         public static string GetVersion()
         {
             string ver = typeof(iba.MainForm).Assembly.GetName().Version.ToString(3);
-            //ver = ver + " BETA4";
+            ver = ver + " BETA2";
             return ver;
         }
 
