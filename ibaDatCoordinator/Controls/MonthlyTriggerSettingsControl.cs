b6c7c560aeaac8e37a0bd85f19c0661c2571ecc3
Michael Verschaeve <michael.verschaeve@iba-ag.com>
2019-08-22 16:05:31 +0000

    collapsible groupbox stuff
    svn path=/trunk/; revision=924

--- ibaDatCoordinator/Controls/MonthlyTriggerSettingsControl.cs
+++ ibaDatCoordinator/Controls/MonthlyTriggerSettingsControl.cs
@@ -16,12 +16,12 @@ namespace iba.Controls
         public MonthlyTriggerSettingsControl()
         {
             InitializeComponent();
-            m_cbMonths.Init((new string[] { iba.Properties.Resources.SelectAllMonths }).Concat(System.Globalization.DateTimeFormatInfo.CurrentInfo.MonthNames.Take(12)), true);
+            m_cbMonths.Init((new string[] { iba.Properties.Resources.SelectAllMonths }).Concat(System.Threading.Thread.CurrentThread.CurrentUICulture.DateTimeFormat.MonthNames.Take(12)), true);
             m_cbDays.min = 1;
             m_cbDays.max = 31;
             m_cbDays.Init(Enumerable.Range(1, 31).Select(n => n.ToString()).Concat(new string[] { iba.Properties.Resources.Last }), false);
             m_cbOnPart1.Init(new string[] { iba.Properties.Resources.First, iba.Properties.Resources.Second, iba.Properties.Resources.Third, iba.Properties.Resources.Fourth, iba.Properties.Resources.Last },false);
-            m_cbOnPartWeekday.Init((new string[] { iba.Properties.Resources.SelectAllDays }).Concat(System.Globalization.DateTimeFormatInfo.CurrentInfo.DayNames.Take(7)), true);
+            m_cbOnPartWeekday.Init((new string[] { iba.Properties.Resources.SelectAllDays }).Concat(System.Threading.Thread.CurrentThread.CurrentUICulture.DateTimeFormat.DayNames.Take(7)), true);
             m_cbOnPartWeekday.DropDownHeigth = 100;
         }
 
