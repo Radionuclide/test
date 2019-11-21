8b902f413ba6b5e959d0a4dfb88045399378aef4
Roman Kolesnik <lesnik4u@yandex.ru>
2019-11-21 14:00:13 +0000

    Added PasswordEditForm form and PasswordEditWithConfirmation control to the project (copied from PDA).
    svn path=/branches/opcua/; revision=1036

---
+++ ibaDatCoordinator/Controls/PasswordEditWithConfirmation.Designer.cs
@@ -0,0 +1,69 @@
+﻿namespace iba.Controls
+{
+    partial class PasswordEditWithConfirmation
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(PasswordEditWithConfirmation));
+            this.tbPass = new System.Windows.Forms.TextBox();
+            this.btEdit = new System.Windows.Forms.Button();
+            this.toolTip1 = new System.Windows.Forms.ToolTip();
+            this.SuspendLayout();
+            // 
+            // tbPass
+            // 
+            resources.ApplyResources(this.tbPass, "tbPass");
+            this.tbPass.Name = "tbPass";
+            this.tbPass.UseSystemPasswordChar = true;
+            // 
+            // btEdit
+            // 
+            resources.ApplyResources(this.btEdit, "btEdit");
+            this.btEdit.Name = "btEdit";
+            this.toolTip1.SetToolTip(this.btEdit, resources.GetString("btEdit.ToolTip"));
+            this.btEdit.UseVisualStyleBackColor = true;
+            this.btEdit.Click += new System.EventHandler(this.btEdit_Click);
+            // 
+            // PasswordEditWithConfirmation
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.btEdit);
+            this.Controls.Add(this.tbPass);
+            this.Name = "PasswordEditWithConfirmation";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbPass;
+        private System.Windows.Forms.Button btEdit;
+        private System.Windows.Forms.ToolTip toolTip1;
+    }
+}
