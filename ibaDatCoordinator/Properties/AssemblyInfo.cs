20ea918ea929dacfd22e7585c7c713aee559033f
Lawrence De Mol <lawrence.demol@iba-ag.com>
2020-04-29 14:25:15 +0000

    [2.3.0 Beta 14]
    svn path=/trunk/; revision=1117

--- ibaDatCoordinator/Properties/AssemblyInfo.cs
+++ ibaDatCoordinator/Properties/AssemblyInfo.cs
@@ -43,7 +43,7 @@ namespace iba
         public static string GetVersion()
         {
             string ver = typeof(iba.MainForm).Assembly.GetName().Version.ToString(3);
-            ver = ver + " BETA13";
+            ver = ver + " BETA14";
             return ver;
         }
 
