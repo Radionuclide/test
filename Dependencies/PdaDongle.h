e72569d0de35a8063468093d8a9c49dcfa84edc3
Nic Gorleer <nic.gorleer@iba-ag.com>
2020-09-16 14:45:24 +0000

    Fixed crash after updating dongle library
    svn path=/trunk/; revision=1160

--- Dependencies/PdaDongle.h
+++ Dependencies/PdaDongle.h
@@ -18,16 +18,52 @@
 #define PASSWORD_INDEX_PDA 0
 #define PASSWORD_INDEX_CAM 1
 #define PASSWORD_INDEX_LOGIC 2
+#define PASSWORD_INDEX_HD 3
+
+#define EUP_INDEX_PDA 0
+#define EUP_INDEX_CAM 1
+#define EUP_INDEX_LOGIC 2
+#define EUP_INDEX_HD 3
+#define EUP_INDEX_ROTATE 4
+#define EUP_INDEX_VISION 5
+#define EUP_INDEX_DAVIS 6
+#define EUP_INDEX_7 7
+#define EUP_INDEX_8 8
+#define EUP_INDEX_9 9
+//#define EUP_INDEX_10 10
+//#define EUP_INDEX_11 11
+//#define EUP_INDEX_12 12
+//#define EUP_INDEX_13 13
+//#define EUP_INDEX_14 14
+//#define EUP_INDEX_15 15
+
+// DongleLib, Dongle.Net.dll and DongleViewer must be recompiled when any EUP_BIT_* define was changed
+#define EUP_BIT_PDA 0		// 16
+#define EUP_BIT_CAM 91
+#define EUP_BIT_LOGIC 0	// 103
+#define EUP_BIT_HD 83
+#define EUP_BIT_ROTATE 160
+#define EUP_BIT_VISION 90
+#define EUP_BIT_DAVIS 192
+#define EUP_BIT_7 0
+#define EUP_BIT_8 0
+#define EUP_BIT_9 0
+#define EUP_BIT_10 0
+#define EUP_BIT_11 0
+#define EUP_BIT_12 0
+#define EUP_BIT_13 0
+#define EUP_BIT_14 0
+#define EUP_BIT_15 0
 
 #pragma pack (push,1)
 
-typedef struct
+typedef struct _DemoLicense
 {
 	unsigned short Index;
 	unsigned char Value;
 } DemoLicense;
 
-typedef struct
+typedef struct _ChangeLogEntry
 {
 	char User[16];
 	unsigned long Timestamp;
@@ -41,19 +77,29 @@ struct DongleContents {
 	unsigned short	day;
 	unsigned short	month;
 	unsigned short	year;
-	short			limit;
-	short			demolimit;
-	unsigned char	multiOptions[2][64];
-	unsigned char	cameraActivation[128];
+	short			limit;			//-1: expired, 0: unlimited, >0: days remainining
+	short			demolimit;		//-1: expired, 0: unlimited, >0: days remainining
+  union
+  {
+	  unsigned char	multiOptions[4][64];
+	  unsigned char	multiOptions2[256];
+  };
 	__int64			lastAccessTime;
 	DemoLicense		demos[8];
 	ChangeLogEntry	log[8];
-	short type;
+	short           type;
+	unsigned char	demosUsed;
+	unsigned char	fwMajor;
+	unsigned char   fwMinor;
+	unsigned short  eup[16];		// 16 * 2 byte -> 16 entries for days since 1.1.2016 => 179 Years until 2195
+
+	char            eupState;		//0: EUP is not active, 1: EUP is ok, 2: EUP not ok but in grace period, 3: EUP not ok and grace period expired, 4: EUP disabled by optout
+	char            eupLimit;		//-1: expired, 0: undefined, >0: days remainining
 };
 
 #pragma pack (pop)
 
-enum  {
+enum {
 	E_NODONGLE = 0,
 	E_USBMPI   = 1,
 	E_SERIAL   = 2,
@@ -64,6 +110,33 @@ enum  {
 
 #define _WINSOCKAPI_
 #include <windows.h>
+
+//=====================================================================================================================
+//forward functions to enable usb notifications. ie. in dongle upgrade
+//dwNotificationType
+#define USB_NOTIFY_BOX_ATTACHED 1
+#define USB_NOTIFY_BOX_REMOVED  2
+// network box changed
+#define USB_NOTIFY_BOX_CHANGED  3
+
+typedef struct
+{
+	void* pNotificationParam;
+	DWORD dwNotificationID;
+	DWORD dwNotificationType;
+} USB_NOTIFY_DATA;
+
+typedef DWORD CALLBACK F_USB_NOTIFY_CALLBACK(USB_NOTIFY_DATA NotificationData);
+
+DWORD WINAPI USB_RegisterNotificationCallback(F_USB_NOTIFY_CALLBACK* fNotify, void* pParam);
+DWORD WINAPI USB_UnregisterNotificationCallback(F_USB_NOTIFY_CALLBACK* fNotify);
+//#ifdef WIN32
+DWORD WINAPI USB_RegisterNotificationMessage(HWND hWnd, UINT message, LPARAM lParam);
+DWORD WINAPI USB_UnregisterNotificationMessage(HWND hWnd);
+//#endif // WIN32
+
+//=====================================================================================================================
+
 
 enum DongleError
 {	
@@ -97,13 +170,13 @@ enum DongleTimeLimitMode
 	NoLimits   = 2     // Don't apply any limits just return the raw values of all dongle licenses
 };
 
-#define NR_MULTI_OPTIONS 128
+#define NR_MULTI_OPTIONS 256
 
 //Abstract base class for all dongle types
 class Dongle
 {
 public:
-	Dongle(){};
+	Dongle() { eupIndex = -1; buildDate = 0; };
 	virtual ~Dongle(){};
 
 	virtual TestDongleResult TestDongle(unsigned char* romData) = 0;
@@ -114,18 +187,32 @@ public:
 
 	virtual char * GetType() = 0;
 
-	virtual bool ReadDongleContents(DongleContents* pContent, DongleReadMode readMode) = 0;
+	virtual bool ReadDongleContents(DongleContents* pContent, DongleReadMode readMode, DongleTimeLimitMode limitMode = AllLimits) = 0;
+	void SetEup(int eupIndex, unsigned short buildDate);
+
+#if !defined(DONGLE_LITE) || defined(DONGLE_DEMO)
+	virtual bool WriteDemoLicenses (DemoLicense demos[8], int limitInDays, unsigned short day, unsigned short month, unsigned short year) = 0;
+#endif
 
 #ifndef DONGLE_LITE	
 	virtual int InitDongle() = 0;
 
 	virtual bool WriteDongleContents(DongleContents* pContent) = 0;
-
 #endif
+
 
 protected:
 	//Checks and also sets back the time limits (decrements) and the lastaccesstime for writing back
-	bool CheckTimeLimits(short* globalTimeLimit, short* demoTimeLimit, __int64* lastAccessTime);
+	bool CheckTimeLimits(short* globalTimeLimit, short* demoTimeLimit, char* eupTimeLimit, __int64* lastAccessTime);
+
+	bool AreDemoLicensesValid(DemoLicense* demos, unsigned char demosUsed);
+	unsigned char UpdateDemoUsedMask(DemoLicense* demos, unsigned char demosUsed);
+
+	//EUP is not active when eupIndex < 0 or buildData = 0
+	int eupIndex;
+	// release date in days since 1.1.2016
+	unsigned short buildDate;
+
 };
 
 
@@ -136,15 +223,24 @@ public:
 	PdaDongle(HMODULE resMod = NULL, bool rescanUSB = false, bool noUsbInstall = false);
 	virtual ~PdaDongle();
 
+	static void Uninitialize(); //Optional function to uninitialize the SmarXOS library
+
 	static char * GetLibVersion();
 	char * GetType();
 
 	int TestDongle();
 	TestDongleResult TestDongle(unsigned char* romData);
 
+
+	void SetEup(int eupIndex, unsigned short buildDate);
 	bool ReadDongleContents(DongleContents* pContent, DongleReadMode readMode = Default, DongleTimeLimitMode limitMode = AllLimits);
 
 	bool WritePasswordForIndex (char* newPassword, int index);
+
+
+#if !defined(DONGLE_LITE) || defined(DONGLE_DEMO)
+	bool WriteDemoLicenses(DemoLicense demos[8], int limitInDays, unsigned short day, unsigned short month, unsigned short year);
+#endif
 
 #ifndef DONGLE_LITE
 	int InitDongle();
@@ -162,10 +258,11 @@ private:
 	void CreateDongle(int dongleType);
 	int AutoDetect();
 
-	Dongle * dongle;
+	Dongle* dongle;
 	HANDLE hDongleAccess;
 
 	void ApplyTimeLimitsCorrections(DongleContents* pContent, bool bDemoOnly);
+
 };
 
 #ifdef _SERVICE
@@ -178,16 +275,24 @@ class CLicenseClientSocket;
 class CibaLicense
 {
 public:
-	CibaLicense();
+	CibaLicense(int eupIndex = -1, unsigned short buildDate = 0);
 	~CibaLicense();
 
+	unsigned short GetEup();		// Get eup days set at index; return 0 indicates error or not set
+	char GetEupState();
 	BOOL HasLicense(DWORD option);
 
 private:
 	BYTE options[NR_MULTI_OPTIONS];
+	int eupIndex;
+	unsigned short buildDate;
+	char eupState;
+	char eupLimit;
+	unsigned short eupValue;
 	CLicenseClientSocket* clientSocket;
 	BOOL IsServiceRunning();
 	void ReadDongle();
+
 };
 #endif
 
@@ -195,15 +300,18 @@ private:
 
 #ifdef IBA_TRACE_ENABLED
 
-void Trace(const char *fmt, ...);
+void DongleTrace(const char *fmt, ...);
+void DongleTraceFile(const char *fmt, ...);
 void DumpMemory(char* msg, unsigned char* pMem, int offset, int length);
 
-#define IBA_TRACE(fmt, ...) Trace(fmt, __VA_ARGS__)
+#define IBA_TRACE(fmt, ...) DongleTrace(fmt, __VA_ARGS__)
+#define IBA_TRACE_FILE(fmt, ...) DongleTraceFile(fmt, __VA_ARGS__)
 #define IBA_DUMPMEM(msg, pMem, offset, length) DumpMemory(msg, pMem, offset, length)
 
 #else
 
 #define IBA_TRACE(fmt, ...) 
+#define IBA_TRACE_FILE(fmt, ...) 
 #define IBA_DUMPMEM(msg, pMem, offset, length) 
 
 #endif
