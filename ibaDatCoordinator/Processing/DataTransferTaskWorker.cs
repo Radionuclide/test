95a64ec12cdacc7f01280c66e7a48a3120e978ed
Alexei Carasiov <alexei.carasiov@iba-ag.com>
2021-06-28 09:40:15 +0000

    add basic client
    svn path=/branches/datatransferservice/; revision=1482

---
+++ ibaDatCoordinator/Processing/DataTransferTaskWorker.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using iba.Processing.IbaGrpc;
+
+namespace iba.Processing
+{
+    class DataTransferTaskWorker
+    {
+        public readonly GrpcClient Server = new GrpcClient();
+
+    }
+}
---
+++ ibaDatCoordinator/Processing/IbaGrpc/GrpcClient.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Grpc.Core;
+using Messages.V1;
+
+namespace iba.Processing.IbaGrpc
+{
+    class GrpcClient
+    {
+        
+    }
+}
