a2e37c4d99f1b9ad1c81311bba00dd34a78aea5d
Alexei Carasiov <alexei.carasiov@iba-ag.com>
2021-06-28 09:40:13 +0000

    clean up gui
    svn path=/branches/datatransferservice/; revision=1481

--- ibaDatCoordinator/Controls/DataTransferTaskControl.cs
+++ ibaDatCoordinator/Controls/DataTransferTaskControl.cs
@@ -20,7 +20,6 @@ namespace iba.Controls
         public DataTransferTaskControl()
         {
             InitializeComponent();
-            ConfigureSearchFileTextBox();
         }
 
         public void LeaveCleanup()
@@ -41,42 +40,15 @@ namespace iba.Controls
 
             m_tbServer.Text = m_data.Server;
             m_tbPort.Text = m_data.Port;
-            m_tbUsername.Text = m_data.Username;
-            m_tbPassword.Text = m_data.Password;
             m_tbRemotePath.Text = m_data.RemotePath;
-            m_tbPathToPrivateKey.Text = m_data.PathToPrivateKey;
-            m_tbPathToCertificate.Text = m_data.PathToCertificate;
-
-            m_cbProtocol.SelectedIndex = (int)m_data.Protocol;
-            m_cbEncryption.SelectedIndex = (int)m_data.EncryptionChoice;
-            m_cmbMode.SelectedIndex = (int)m_data.Mode;
-
-
-            m_rbDatFile.Checked = m_data.WhatFileUpload == DataTransferTaskData.WhatFileUploadEnum.DATFILE;
-            m_rbPrevOutput.Checked = m_data.WhatFileUpload == DataTransferTaskData.WhatFileUploadEnum.PREVOUTPUT;
-
-            m_chkAnonymous.Checked = m_data.Anonymous;
-            m_chkAcceptAnySshHostKey.Checked = m_data.AcceptAnySshHostKey;
-            m_chkAcceptAnyTlsCertificate.Checked = m_data.AcceptAnyTlsCertificate;
         }
 
         public void SaveData()
         {
             m_data.Server = m_tbServer.Text;
             m_data.Port = m_tbPort.Text;
-            m_data.Username = m_tbUsername.Text;
-            m_data.Password = m_tbPassword.Text;
             m_data.RemotePath = m_tbRemotePath.Text;
-            m_data.PathToPrivateKey = m_tbPathToPrivateKey.Text;
-            m_data.PathToCertificate = m_tbPathToCertificate.Text;
 
-            m_data.Protocol = (DataTransferTaskData.TransferProtocol)m_cbProtocol.SelectedIndex;
-            m_data.EncryptionChoice = (DataTransferTaskData.EncryptionChoiceEnum)m_cbEncryption.SelectedIndex;
-            m_data.Mode = (DataTransferTaskData.FtpMode)m_cmbMode.SelectedIndex;
-
-            m_data.Anonymous = m_chkAnonymous.Checked;
-            m_data.AcceptAnySshHostKey = m_chkAcceptAnySshHostKey.Checked;
-            m_data.AcceptAnyTlsCertificate = m_chkAcceptAnyTlsCertificate.Checked;
 
             if (m_rbPrevOutput.Checked)
                 m_data.WhatFileUpload = DataTransferTaskData.WhatFileUploadEnum.PREVOUTPUT;
@@ -87,114 +59,6 @@ namespace iba.Controls
                 TaskManager.Manager.ReplaceConfiguration(m_data.ParentConfigurationData);
         }
 
-        private void m_cbProtocol_SelectedIndexChanged(object sender, EventArgs e)
-        {
-            var selectedProtocol = (string)((ComboBox)sender).SelectedItem;
-            var selectedEncryption = (string)m_cbEncryption.SelectedItem;
-
-            var ftpControls = new List<Control>
-            {
-                m_lbEncryption, m_cbEncryption, m_lbFtpMode, m_cmbMode, m_chkAnonymous
-            };
-
-            ftpControls.ForEach(control => control.Visible = selectedProtocol == "FTP");
-
-            var ftpControlsUnencrypted = new List<Control>
-            {
-                m_chkAcceptAnyTlsCertificate, m_tlpPathtoCertificate
-            };
-
-            ftpControlsUnencrypted.ForEach(control => control.Visible = selectedProtocol == "FTP" && selectedEncryption != "Use only unencrypted FTP (insecure)");
-
-            var sshControls = new List<Control>
-            {
-                m_chkAcceptAnySshHostKey, m_tlpPathToKey
-            };
-
-            sshControls.ForEach(control => control.Visible = selectedProtocol == "SFTP" || selectedProtocol == "SCP");
-
-            SetStandardPorts(selectedProtocol);
-        }
-
-        private void m_cbEncryption_SelectedIndexChanged(object sender, EventArgs e)
-        {
-            var selectedEncryption = (string)((ComboBox)sender).SelectedItem;
-            var selectedProtocol = (string)m_cbProtocol.SelectedItem;
-
-            var ftpEncryptionControls = new List<Control>
-            {
-                m_chkAcceptAnyTlsCertificate, m_tlpPathtoCertificate
-            };
-            ftpEncryptionControls.ForEach(control => control.Visible = selectedProtocol == "FTP" && selectedEncryption != "Use only unencrypted FTP (insecure)");
-
-            var sshEncryptionControls = new List<Control> { m_chkAcceptAnySshHostKey, m_tlpPathToKey };
-            sshEncryptionControls.ForEach(control => control.Visible = selectedProtocol == "SFTP" || selectedProtocol == "SCP");
-
-            SetStandardPorts(selectedProtocol);
-        }
-
-        private void SetStandardPorts(string selectedProtocol)
-        {
-            const string ftp = "21";
-            const string implicitFtp = "990";
-            const string sftp = "22";
-            const string amazonS3 = "443";
-
-            var isStandardPort = m_tbPort.Text == ftp ||
-                                 m_tbPort.Text == sftp ||
-                                 m_tbPort.Text == implicitFtp ||
-                                 m_tbPort.Text == amazonS3;
-
-            if (!isStandardPort)
-                return;
-
-            switch (selectedProtocol)
-            {
-                case "FTP":
-                    m_tbPort.Text = "Require implicit FTP over TLS" == (string)m_cbEncryption.SelectedItem ? implicitFtp : ftp;
-                    break;
-                case "SFTP":
-                case "SCP":
-                    m_tbPort.Text = sftp;
-                    break;
-                case "Amazon S3":
-                    m_tbPort.Text = amazonS3;
-                    break;
-            }
-        }
-
-        private void m_chkAnonymous_CheckedChanged(object sender, EventArgs e)
-        {
-            var checkbox = (CheckBox)sender;
-            const string anonymous = "anonymous";
-
-            if (checkbox.Checked)
-            {
-                m_tbUsername.Text = anonymous;
-                m_tbPassword.Text = anonymous;
-                return;
-            }
-
-            checkbox.Checked = IsAnonymousInserted();
-        }
-
-        private void m_tbUsername_TextChanged(object sender, EventArgs e)
-        {
-            m_chkAnonymous.Checked = IsAnonymousInserted();
-        }
-
-        private void m_tbPassword_TextChanged(object sender, EventArgs e)
-        {
-            m_chkAnonymous.Checked = IsAnonymousInserted();
-        }
-
-        private bool IsAnonymousInserted()
-        {
-            const string anonymous = "anonymous";
-
-            return m_tbUsername.Text == anonymous && m_tbPassword.Text == anonymous;
-        }
-
         private void m_btnCheckConnection_Click(object sender, EventArgs e)
         {
             SaveData();
@@ -203,12 +67,8 @@ namespace iba.Controls
 
             using (var wait = new WaitCursor())
             {
-
-                //var uploadTaskWorker = new UploadTaskWorker(string.Empty, m_data);
-
                 try
                 {
-                    //ok = uploadTaskWorker.TestConnection(out errormessage);
                 }
                 catch (Exception ex)
                 {
@@ -233,55 +93,6 @@ namespace iba.Controls
                 m_btnCheckConnection.Image = iba.Properties.Resources.thumbdown;
             }
             ((Bitmap)m_btnCheckConnection.Image).MakeTransparent(Color.Magenta);
-        }
-
-        private void ConfigureSearchFileTextBox()
-        {
-            var SearchPrivateKeyBtn = new Button();
-            var SearchCertificateBtn = new Button();
-
-            var buttonlist = new List<Button> { SearchCertificateBtn, SearchPrivateKeyBtn };
-
-            buttonlist.ForEach(btn => {
-                btn.Size = new Size(25, m_tbPathToPrivateKey.ClientSize.Height + 2);
-                btn.Dock = DockStyle.Right;
-                btn.Cursor = Cursors.Default;
-                btn.Text = ". . .";
-                btn.FlatStyle = FlatStyle.Flat;
-                btn.ForeColor = Color.Black;
-                btn.Font = new Font("Arial", 6, FontStyle.Bold);
-                btn.FlatAppearance.BorderColor = Color.Black;
-                btn.FlatAppearance.BorderSize = 15;
-                btn.BackColor = Color.DarkGray;
-                btn.FlatAppearance.BorderSize = 0;
-                btn.Click += SearchPrivateKeyBtn_Click;
-            });
-
-
-            m_tbPathToCertificate.Controls.Add(SearchCertificateBtn);
-            m_tbPathToPrivateKey.Controls.Add(SearchPrivateKeyBtn);
-
-            SendMessage(m_tbPathToCertificate.Handle, 0xd3, (IntPtr)2, (IntPtr)(SearchPrivateKeyBtn.Width << 16));
-            SendMessage(m_tbPathToPrivateKey.Handle, 0xd3, (IntPtr)2, (IntPtr)(SearchPrivateKeyBtn.Width << 16));
-        }
-
-        [System.Runtime.InteropServices.DllImport("user32.dll")]
-        private static extern IntPtr SendMessage(IntPtr hWnd, int msg, IntPtr wp, IntPtr lp);
-
-        private void SearchPrivateKeyBtn_Click(object sender, EventArgs e)
-        {
-            openFileDialog1.ShowDialog();
-
-            var filename = openFileDialog1.FileName;
-
-            if (m_cbProtocol.SelectedItem.ToString() == "FTP")
-            {
-                m_tbPathToCertificate.Text = filename;
-            }
-            else
-            {
-                m_tbPathToPrivateKey.Text = filename;
-            }
         }
     }
 }
--- ibaDatCoordinator/Data/DataTransferTaskData.cs
+++ ibaDatCoordinator/Data/DataTransferTaskData.cs
@@ -39,27 +39,6 @@ namespace iba.Data
             set => m_password = value;
         }
 
-        public string PasswordCrypted
-        {
-            get => Crypt.Encrypt(m_password);
-            set => m_password = Crypt.Decrypt(value);
-        }
-        
-        private string m_privateKeyPassphrase = string.Empty;
-        
-        [XmlIgnore]
-        public string PrivateKeyPassphrase
-        {
-            get => m_privateKeyPassphrase;
-            set => m_privateKeyPassphrase = value;
-        }
-
-        public string PrivateKeyPassphraseCrypted
-        {
-            get => Crypt.Encrypt(m_privateKeyPassphrase);
-            set => m_privateKeyPassphrase = Crypt.Decrypt(value);
-        }
-
         private string m_remotePath;
         public string RemotePath
         {
@@ -67,25 +46,10 @@ namespace iba.Data
             set => m_remotePath = value;
         }
 
-        private string m_tlsCertificateFingerprint;
-        public string TlsCertificateFingerprint
-        {
-            get => m_tlsCertificateFingerprint;
-            set => m_tlsCertificateFingerprint = value;
-        }
-
-        private string m_sshHostKeyFingerprint;
-        public string SshHostKeyFingerprint
-        {
-            get => m_sshHostKeyFingerprint;
-            set => m_sshHostKeyFingerprint = value;
-        }
-
         public DataTransferTaskData(ConfigurationData parent)
             : base(parent)
         {
             m_name = iba.Properties.Resources.DataTransferTaskTitle;
-            Port = "21";
             RemotePath = "/";
         }
 
@@ -99,72 +63,6 @@ namespace iba.Data
             set => m_whatFileUpload = value;
         }
 
-        public enum EncryptionChoiceEnum { None, ExplicitTls, ImplicitTls }
-
-        private EncryptionChoiceEnum m_encryptionChoice;
-
-        public EncryptionChoiceEnum EncryptionChoice
-        {
-            get => m_encryptionChoice;
-            set => m_encryptionChoice = value;
-        }
-
-        public enum TransferProtocol { Ftp, Sftp, Scp, AmazonS3}
-
-        private TransferProtocol m_transferProtocol;
-
-        public TransferProtocol Protocol
-        {
-            get => m_transferProtocol;
-            set => m_transferProtocol = value;
-        }
-        public enum FtpMode { Passive, Active}
-
-        private FtpMode m_ftpMode;
-        public FtpMode Mode
-        {
-            get => m_ftpMode;
-            set => m_ftpMode = value;
-        }
-
-        private bool m_anonymous;
-
-        public bool Anonymous
-        {
-            get => m_anonymous;
-            set => m_anonymous = value;
-        }
-        private string m_pathToPrivateKey;
-
-        public string PathToPrivateKey
-        {
-            get => m_pathToPrivateKey;
-            set => m_pathToPrivateKey = value;
-        }
-        private bool m_acceptAnySshHostKey;
-
-        public bool AcceptAnySshHostKey
-        {
-            get => m_acceptAnySshHostKey;
-            set => m_acceptAnySshHostKey = value;
-        }
-
-        private bool m_acceptAnyTlsCertificate;
-
-        public bool AcceptAnyTlsCertificate
-        {
-            get => m_acceptAnyTlsCertificate;
-            set => m_acceptAnyTlsCertificate = value;
-        }
-
-        private string m_pathToCertificate;
-
-        public string PathToCertificate
-        {
-            get => m_pathToCertificate;
-            set => m_pathToCertificate = value;
-        }
-
         public DataTransferTaskData()
             : this(null)
         {
@@ -176,22 +74,9 @@ namespace iba.Data
             cd.m_server = m_server;
             cd.m_username = m_username;
             cd.m_password = m_password;
-            cd.m_privateKeyPassphrase = m_privateKeyPassphrase;
             cd.m_port = m_port;
             cd.m_remotePath = m_remotePath;
-            cd.m_tlsCertificateFingerprint = m_tlsCertificateFingerprint;
-            cd.m_encryptionChoice = m_encryptionChoice;
-            cd.m_transferProtocol = m_transferProtocol;
-            cd.m_ftpMode = m_ftpMode;
             cd.m_whatFileUpload = m_whatFileUpload;
-            cd.m_anonymous = m_anonymous;
-            cd.m_acceptAnySshHostKey = m_acceptAnySshHostKey;
-            cd.m_acceptAnyTlsCertificate = m_acceptAnyTlsCertificate;
-            cd.SshHostKeyFingerprint = SshHostKeyFingerprint;
-            cd.TlsCertificateFingerprint = TlsCertificateFingerprint;
-            cd.m_pathToCertificate = m_pathToCertificate;
-            cd.m_pathToPrivateKey = m_pathToPrivateKey;
-            
             return cd;
         }
 
@@ -205,20 +90,8 @@ namespace iba.Data
                 other.m_server == m_server &&
                 other.m_username == m_username &&
                 other.m_password == m_password &&
-                other.m_privateKeyPassphrase == m_privateKeyPassphrase &&
                 other.m_port == m_port &&
-                other.m_remotePath == m_remotePath &&
-                other.m_tlsCertificateFingerprint == m_tlsCertificateFingerprint &&
-                other.m_encryptionChoice == m_encryptionChoice &&
-                other.m_transferProtocol == m_transferProtocol &&
-                other.m_ftpMode == m_ftpMode &&
-                other.m_anonymous == m_anonymous &&
-                other.m_acceptAnySshHostKey == m_acceptAnySshHostKey &&
-                other.m_acceptAnyTlsCertificate == m_acceptAnyTlsCertificate &&
-                other.m_sshHostKeyFingerprint == m_sshHostKeyFingerprint &&
-                other.m_tlsCertificateFingerprint == m_tlsCertificateFingerprint &&
-                other.m_pathToCertificate == m_pathToCertificate &&
-                other.m_pathToPrivateKey == m_pathToPrivateKey;
+                other.m_remotePath == m_remotePath;
         }
     }
 }
